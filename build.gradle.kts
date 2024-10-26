a8e68758f48c504ab523ab5382591f8a08094e73
ankitkumar <ankit.kumar071460@gmail.com>
2024-10-27 00:29:23 +0530

    add publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,12 @@
+import java.net.URI
+
 plugins {
     id("java")
+    id("maven-publish")
 }
 
 group = "org.dlms"
-version = "1.0-SNAPSHOT"
+version = "1.0.0"
 
 repositories {
     mavenCentral()
@@ -14,6 +17,44 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
-tasks.test {
-    useJUnitPlatform()
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+            pom {
+                name = "DLMS Protos"
+                description = "A proto library for dlms schemas."
+                url.set("https://github.com/bxute/dlms-schemas")
+
+                licenses {
+                    license {
+                        name.set("MIT License")
+                        url.set("https://opensource.org/licenses/MIT")
+                    }
+                }
+
+                developers {
+                    developer {
+                        id.set("org.dlms")
+                        name.set("Ankit Kumar")
+                        email.set("ankit.kumar071460@gmail.com")
+                    }
+                }
+
+                scm {
+                    url.set("https://github.com/bxute/dlms-schemas")
+                }
+            }
+        }
+    }
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = URI.create("https://maven.pkg.github.com/bxute/dlms-schemas") // For release
+            credentials {
+                username = (project.findProperty("gpr.user") ?: "").toString()
+                password = (project.findProperty("gpr.token") ?: "").toString()
+            }
+        }
+    }
 }
